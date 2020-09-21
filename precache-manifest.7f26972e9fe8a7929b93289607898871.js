5dca0f4c1ccc6d7341cc94921d8cfd43151fa571
Frosty273 <kelvinngor387@gmail.com>
2020-09-21 21:36:19 +1200

    Updates

---
+++ precache-manifest.7f26972e9fe8a7929b93289607898871.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb85bbd1ddb5b4dcc463271206d4140e",
+    "url": "/Pomodoro-Todo/index.html"
+  },
+  {
+    "revision": "8c1d2c73e8f1a5b094bf",
+    "url": "/Pomodoro-Todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abaac5a4ba1461a89c98",
+    "url": "/Pomodoro-Todo/static/js/2.f24002f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Todo/static/js/2.f24002f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1d2c73e8f1a5b094bf",
+    "url": "/Pomodoro-Todo/static/js/main.9f638a79.chunk.js"
+  },
+  {
+    "revision": "017eb3c1691f44fc4be3",
+    "url": "/Pomodoro-Todo/static/js/runtime-main.cc37155b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pomodoro-Todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
