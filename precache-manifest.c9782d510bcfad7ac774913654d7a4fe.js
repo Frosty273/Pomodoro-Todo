720557064f7c776cb89e987c81ee5d55ca31962c
Frosty273 <kelvinngor387@gmail.com>
2020-09-22 01:17:50 +1200

    Updates

---
+++ precache-manifest.c9782d510bcfad7ac774913654d7a4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbe8e35ff8d1234ced70763f3207994",
+    "url": "/Pomodoro-Todo/index.html"
+  },
+  {
+    "revision": "a1eb0eb392e135763b99",
+    "url": "/Pomodoro-Todo/static/css/main.cf4587bd.chunk.css"
+  },
+  {
+    "revision": "d97884b50f716e30cbb1",
+    "url": "/Pomodoro-Todo/static/js/2.cb460fde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Pomodoro-Todo/static/js/2.cb460fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1eb0eb392e135763b99",
+    "url": "/Pomodoro-Todo/static/js/main.b5829604.chunk.js"
+  },
+  {
+    "revision": "017eb3c1691f44fc4be3",
+    "url": "/Pomodoro-Todo/static/js/runtime-main.cc37155b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pomodoro-Todo/precache-manifest.619d343e4c332c71352fd65077b73ef2.js"
+  "/Pomodoro-Todo/precache-manifest.c9782d510bcfad7ac774913654d7a4fe.js"
 );
 
 self.addEventListener('message', (event) => {
